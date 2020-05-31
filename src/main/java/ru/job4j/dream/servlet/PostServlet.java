0647c838235e210f931d8f0234504a5699773f86
Sekator778 <sekator778@gmail.com>
2020-05-31 09:09:43 +0300

    убрать коменты

--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -10,23 +10,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- *
- */
 public class PostServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
-        /* Метод getParameter позволяет получить значения в запросе */
         PsqlStore.instOf().save(
                 new Post(
                         Integer.parseInt(req.getParameter("id")),
                         req.getParameter("name")
                 )
-        );        /*После того, как сервлет выполнил свою работу мы просим сервер отправить другой запрос,
-         но уже к странице posts.jsp.
-         */
+        );
         resp.sendRedirect(req.getContextPath() + "/posts.do");
     }
 
--- src/main/webapp/post/edit.jsp
+++ src/main/webapp/post/edit.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: sekator
-  Date: 29.05.2020
-  Time: 16:40
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html; charset=UTF-8" %>
 <%@ page import="ru.job4j.dream.model.Post" %>
 <%@ page import="ru.job4j.dream.store.PsqlStore" %>
