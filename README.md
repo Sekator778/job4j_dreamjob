f08d7171dc47429e9a7cc1bf88e9800c37b7a4b3
Sekator778 <sekator778@gmail.com>
2020-06-09 22:05:21 +0300

    Mock2

--- README.md
+++ README.md
@@ -2,19 +2,20 @@
 <h3>
 Приложение биржа работы.
 </h3>
+[![Build Status](https://travis-ci.org/Sekator778/job4j_dreamjob.svg?branch=master)](https://travis-ci.org/Sekator778/job4j_dreamjob)
+[![codecov](https://codecov.io/gh/Sekator778/job4j_dreamjob/branch/master/graph/badge.svg)](https://codecov.io/gh/Sekator778/job4j_dreamjob)
 <h4>
 В системе будут два типа пользователей: кандидаты и кадровики. Кандидаты будут публиковать резюме. Кадровики будут публиковать вакансии о работе.
 <tr>
 Кандидаты могут откликнуться на вакансию. Кадровик может пригласить на вакансию кандидата.
 </h4>
 
-[![Build Status](https://travis-ci.org/Sekator778/job4j_dreamjob.svg?branch=master)](https://travis-ci.org/Sekator778/job4j_dreamjob)
 
 <h3>Использованные средства:</h3>
 <ul>
-<li>Bootstrap - для реализации UI</li>
-<li>Junit, powermock - тестирование UI</li>
-<li>J2EE - JPA, Servlet</li>
-<li>Apache Tomcat - контейнер сервлетов UI</li>
-<li>POSTGRESQL - база данных</li>
+<li><i>Bootstrap - для реализации UI</i>i></li>
+<li><i>Junit, powermock - тестирование UI</i></li>
+<li><i>J2EE - JPA, Servlet</i></li>
+<li><i>Apache Tomcat - контейнер сервлетов UI</i></li>
+<li><i>POSTGRESQL - база данных</i></li>
 </ul>
--- src/main/test/ru/job4j/dream/servlet/CandidateServletTest.java
+++ src/main/test/ru/job4j/dream/servlet/CandidateServletTest.java
@@ -1,7 +1,10 @@
 package ru.job4j.dream.servlet;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.PsqlStore;
 import ru.job4j.dream.store.Store;
 import ru.job4j.dream.store.StoreSub;
@@ -10,6 +13,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -34,5 +40,22 @@ public class CandidateServletTest {
         when(req.getParameter("name")).thenReturn("Alex");
         new RegServlet().doPost(req, resp);
         assertThat(store.findAllUsers().iterator().next().getName(), is("Alex"));
+    }
+
+    @Ignore
+    public void whenAddPostThenStoreIt() throws ServletException, IOException {
+        Store store = new StoreSub();
+        PowerMockito.mockStatic(PsqlStore.class);
+        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        when(req.getParameter("name")).thenReturn("Alex");
+        new PostServlet().doPost(req, resp);
+        Collection<Post> map = new ArrayList<>();
+               map = store.findAllPosts();
+        Post post = map.iterator().next();
+        System.out.println("post " + post.getName());
+        System.out.println("map have = " + map.toString());
+        assertThat(map.iterator().next().getName(), is("Alex"));
     }
 }
