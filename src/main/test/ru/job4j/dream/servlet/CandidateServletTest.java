6d62797c4672d442a95dc9d3623d58bcad64885e
Sekator778 <sekator778@gmail.com>
2020-06-09 22:20:40 +0300

    Mock2

--- src/main/test/ru/job4j/dream/servlet/CandidateServletTest.java
+++ src/main/test/ru/job4j/dream/servlet/CandidateServletTest.java
@@ -3,7 +3,6 @@ package ru.job4j.dream.servlet;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.PsqlStore;
 import ru.job4j.dream.store.Store;
@@ -15,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
 
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -49,10 +47,10 @@ public class CandidateServletTest {
         PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-        when(req.getParameter("name")).thenReturn("Alex");
+        when(req.getParameter("id")).thenReturn("1");
         new PostServlet().doPost(req, resp);
         Collection<Post> map = new ArrayList<>();
-               map = store.findAllPosts();
+        map = store.findAllPosts();
         Post post = map.iterator().next();
         System.out.println("post " + post.getName());
         System.out.println("map have = " + map.toString());
