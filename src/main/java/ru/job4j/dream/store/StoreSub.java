131d77cff2519a35da07cdc8962cd871fd3ee7b0
Sekator778 <sekator778@gmail.com>
2020-06-10 11:18:35 +0300

    no this

--- src/main/java/ru/job4j/dream/store/StoreSub.java
+++ src/main/java/ru/job4j/dream/store/StoreSub.java
@@ -16,67 +16,67 @@ public class StoreSub implements Store {
 
     @Override
     public Collection<Post> findAllPosts() {
-        return this.postMap.values();
+        return postMap.values();
     }
 
     @Override
     public Collection<Candidate> findAllCandidates() {
-        return this.candidateMap.values();
+        return candidateMap.values();
     }
 
     @Override
     public void save(Post post) {
-        post.setId(this.idPost++);
+        post.setId(idPost++);
         postMap.put(post.getId(), post);
     }
 
     @Override
     public void save(Candidate candidate) {
-        candidate.setId(this.idCandidate++);
+        candidate.setId(idCandidate++);
         candidateMap.put(candidate.getId(), candidate);
     }
 
     @Override
     public Post findById(int id) {
-        return this.postMap.get(id);
+        return postMap.get(id);
     }
 
     @Override
     public Candidate findByIdCandidate(int id) {
-        return this.candidateMap.get(id);
+        return candidateMap.get(id);
     }
 
     @Override
     public void update(Post post) {
-        this.postMap.get(post.getId()).setName(post.getName());
+        postMap.get(post.getId()).setName(post.getName());
     }
 
     @Override
     public void update(Candidate candidate) {
-        this.candidateMap.get(candidate.getId()).setName(candidate.getName());
+        candidateMap.get(candidate.getId()).setName(candidate.getName());
 
     }
 
     @Override
     public void deleteCandidate(int id) {
-        this.candidateMap.remove(id);
+        candidateMap.remove(id);
     }
 
     @Override
     public void addUser(User user) {
-        user.setId(this.idUser++);
-        this.userList.add(user);
+        user.setId(idUser++);
+        userList.add(user);
     }
 
     @Override
     public List<User> findAllUsers() {
-        return this.userList;
+        return userList;
     }
 
     @Override
     public User findUserByEmail(String email) {
         User result = null;
-        for (User u : this.userList
+        for (User u : userList
         ) {
             if (u.getEmail().equals(email)) {
                 result = u;
