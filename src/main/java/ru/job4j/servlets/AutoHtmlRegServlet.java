ad9ce2e39016458a85f7de9801294209439e8270
Sekator778 <sekator778@gmail.com>
2020-06-13 17:42:59 +0300

    как передать в arr

--- src/main/java/ru/job4j/servlets/AutoHtmlRegServlet.java
+++ src/main/java/ru/job4j/servlets/AutoHtmlRegServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 /**
  *
@@ -17,11 +18,15 @@ public class AutoHtmlRegServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
-        String email = req.getParameter("name");
-        String password = req.getParameter("name");
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+        //тут приходит массив send из avtoriz.html надо его тут распарсить пока не знаю как
+        String[] strings = req.getParameterValues("send");
+        System.out.println("send: " + Arrays.toString(strings));
+        String name = req.getParameter("inputName");
+        String email = req.getParameter("inputEmail");
+        String password = req.getParameter("inputPassword");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
+        writer.println("Nice to meet you, " + name + password + email);
         writer.flush();
     }
 }
