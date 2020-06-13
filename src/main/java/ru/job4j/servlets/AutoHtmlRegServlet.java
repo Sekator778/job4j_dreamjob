a303582c2c2fed58c80f55c5fdd64e94ae9a6266
Sekator778 <sekator778@gmail.com>
2020-06-13 12:19:52 +0300

    валидация + ajax

---
+++ src/main/java/ru/job4j/servlets/AutoHtmlRegServlet.java
@@ -0,0 +1,27 @@
+package ru.job4j.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ */
+
+public class AutoHtmlRegServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        String name = req.getParameter("name");
+        String email = req.getParameter("name");
+        String password = req.getParameter("name");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.println("Nice to meet you, " + name);
+        writer.flush();
+    }
+}
