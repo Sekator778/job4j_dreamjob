1ac9a00875d63d3048d8cbe944e085b259a18d2b
Sekator778 <sekator778@gmail.com>
2020-06-13 16:42:37 +0300

    check with radio button

--- src/main/jss/addRow2.js
+++ src/main/jss/addRow2.js
@@ -2,10 +2,19 @@ function addRow2() {
     const name = $('#inputName').val();
     const email = $('#inputEmail').val();
     const desc = $('#inputDescription').val();
+    var value = document.getElementsByName("sex")
+    var sex;
+    if (value[0].checked == true) {
+        sex = 'Female';
+    }
+    if (value[1].checked == true) {
+        sex = 'Male';
+    }
     if ((name || email || desc).length > 0) {
         $('#t2 tr:last').after('<tr><td>' + name + '</td>' +
-            ' <td>' + email + '</td>' +
-            ' <td>' + desc + '</td>' +
-            '</tr>');
+            ' <td>' + email + '</td>'
+            + ' <td>' + desc + '</td>'
+            + ' <td>' + sex + '</td>'
+            + '</tr>');
     }
 }
